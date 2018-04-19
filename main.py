88b072b20cf80efb2a0d0ab6f13499a3cb3c93e7
Henry Lim <henrylim.everything@gmail.com>
2018-04-19 16:13:10 +0800

    change to wtmkl18

--- main.py
+++ main.py
@@ -2,17 +2,8 @@ import webapp2
 
 class SlashRedirect(webapp2.RequestHandler):
   def get(self, *args, **kwargs):
-    self.redirect('//' + self.request.host + '/tfxkl18/')
-
-class TensorflowExteneded(webapp2.RequestHandler):
-  def get(self, *args, **kwargs):
-    path = self.request.path_qs.split('/')
-    if (path[1] != 'tfxkl'):
-        self.redirect('https://www.gdgkl.org')
-    else:
-        self.redirect('//' + self.request.host + '/tfxkl18/' + '/'.join(path[2:]))
+    self.redirect('//' + self.request.host + '/wtmkl18/')
 
 app = webapp2.WSGIApplication([
-    ('/tfxkl18.*', SlashRedirect),
-    ('/tfxkl.*', TensorflowExteneded)
+    ('/wtmkl18.*', SlashRedirect)
 ], debug=True)
